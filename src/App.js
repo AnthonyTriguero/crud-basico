f7e47502a1c7c9c36a2a9d44e43ba65470dcc64c
Saber <anthony.2108@hotmail.com>
2022-05-04 11:49:58 -0500

    v6-api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import  Listar  from "./componentes/Listar";
+import Crear from "./componentes/Crear";
+import Editar from "./componentes/Editar";
 import './App.css';
+import {BrowserRouter,Route,Routes,Link} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <nav className="navbar navbar-expand navbar-light bg-light">
+                <div className="nav navbar-nav">
+                    <Link className="nav-item nav-link active" to={"/"}>Sistema </Link>
+                    <Link className="nav-item nav-link" to={"/crear"}>Crear Empleado</Link>
+                </div>
+            </nav>
+     <div className="container">
+       <br></br>
+      <Routes>
+      <Route path="/" element={<Listar/>}/>
+      <Route path="crear" element={<Crear/>}/>
+      <Route path="editar" element={<Editar/>}/>
+
+      </Routes>
+      </div>
+    </BrowserRouter>
+      
   );
 }
+
 
 export default App;
---
+++ src/componentes/Listar.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+class Listar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { datosCargados:false,
+                        empleados:[]     
+                    
+                    
+                    }
+    }
+    
+
+    CargarDatos(){
+        fetch("http://localhost/empleados/")
+        .then(respuesta=>respuesta.json())
+        .then((datosRespuesta)=>{
+            console.log(datosRespuesta)
+            this.setState({ datosCargados:true, empleados:datosRespuesta })
+            
+        })
+
+
+        .catch(error => console.log(error))
+    }
+
+
+    componentDidMount(){
+        this.CargarDatos();
+    }
+
+    render() { 
+        const{datosCargados, empleados}=this.state
+      
+
+        if (!datosCargados){return(<div>Cargando...</div>);}
+        else{
+        return ( 
+            <div class="card">
+                <div class="card-header">
+                        Lista de empleados
+                </div>
+                <div class="card-body">
+                    <h4 class="card-title">Registro</h4>
+                    <><table className="table">
+                    <><thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Correo</th>
+                            <th>Accion</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {empleados.map(
+                        (empleado)=>(
+                            <tr key={empleado.id}>
+                                
+                                <td scope="row"> {empleado.id} </td>
+                                <td>{empleado.nombre} </td>
+                                <td>{empleado.correo} </td>
+                                
+                                <td>
+                                    <div className="btn-group" role="group" aria-label="">
+                                        <Link  className="btn btn-warning" to={"/editar"}>Editar</Link>
+                                        <button type="button" className="btn btn-danger">Borrar</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        )
+                    )}
+                       
+                            
+                </tbody></>
+                        
+
+                </table></>
+                    
+                </div>
+                <div class="card-footer text-muted">
+                    
+                </div>
+            </div>
+             );
+                }
+    }
+}
+ 
+export default Listar;
+
+
